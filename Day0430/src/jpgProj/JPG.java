109cc8de88cc2a2d8ec6598a1fb6c4d29bbdd29c
SSH <SSH@DESKTOP-3HRH643>
2025-04-30 10:47:50 +0900

    2트
    파일변환

--- Day0430/src/jpgProj/JPG.java
+++ Day0430/src/jpgProj/JPG.java
@@ -1,7 +1,9 @@
 package jpgProj;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import net.coobird.thumbnailator.Thumbnails;
 
 public class JPG {
 
@@ -19,6 +21,29 @@ public class JPG {
 			else
 				break;
 		}
+		
+		System.out.println("변환할 파일의 가로픽셀 입력: ");
+		int width=sc.nextInt();
+		System.out.println("변환할 파일의 세로픽셀 입려력 ");
+		int height=sc.nextInt();
+		System.out.println("변환할 파일명 입력: ");
+		sc.nextLine();
+		String reName=sc.nextLine();
+		
+		String outputFile = reName;
+		
+		try {
+			Thumbnails.of(input)
+			.size(width, height)
+			.outputFormat("jpg")
+			.toFile(new File(outputFile));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			
+		}
+		System.out.println(outputFile+" 변환완료");
 		
 
 	}
